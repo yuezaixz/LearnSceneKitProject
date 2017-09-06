b10130b4df308ee285db04a99636105a355f7a96
yuezaixz <303178394@qq.com>
2017-09-06 15:04:20 +0800

    Chapter 1.

---
+++ GeometryFighter/GeometryFighter/GameViewController.swift
@@ -0,0 +1,53 @@
+//
+//  GameViewController.swift
+//  GeometryFighter
+//
+//  Created by 吴迪玮 on 2017/9/6.
+//  Copyright © 2017年 吴迪玮. All rights reserved.
+//
+
+import UIKit
+import QuartzCore
+import SceneKit
+
+class GameViewController: UIViewController {
+
+    var scnView: SCNView!
+    var scnScene: SCNScene!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+        setupScene()
+    }
+    
+    func setupView() {
+        scnView = self.view as! SCNView
+    }
+    
+    func setupScene() {
+        scnScene = SCNScene()
+        scnView.scene = scnScene
+    }
+    
+    override var shouldAutorotate: Bool {
+        return true
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .all
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
