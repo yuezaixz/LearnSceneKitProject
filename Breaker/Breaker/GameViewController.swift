4e7d2332e38cae9e4622ef2275f920e32f472d7c
yuezaixz <303178394@qq.com>
2017-09-12 15:38:31 +0800

    create Breaker Project

---
+++ Breaker/Breaker/GameViewController.swift
@@ -0,0 +1,65 @@
+//
+//  GameViewController.swift
+//  Breaker
+//
+//  Created by 吴迪玮 on 2017/9/12.
+//  Copyright © 2017年 吴迪玮. All rights reserved.
+//
+
+import UIKit
+import QuartzCore
+import SceneKit
+
+class GameViewController: UIViewController {
+
+    var scnView: SCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupScene()
+        setupNodes()
+        setupSounds()
+    }
+    
+    func setupScene() {
+        scnView = self.view as! SCNView
+        scnView.delegate = self
+    }
+    
+    func setupNodes() {
+        
+    }
+    
+    func setupSounds() {
+    }
+    
+    //MARK: others
+    
+    override var shouldAutorotate: Bool {
+        return true
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .all
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
+
+extension GameViewController: SCNSceneRendererDelegate {
+    func renderer(_ renderer: SCNSceneRenderer,
+                  updateAtTime time: TimeInterval) {
+    }
+}
